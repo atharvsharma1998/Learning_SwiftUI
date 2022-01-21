3b2a71a69e3b1102dbf94b50b2d9730eb2a5d329
atharvsharma <atharv228.as@gmail.com>
2022-01-22 00:56:01 +0530

    Initial Commit

---
+++ ButtonWithBadge/ButtonWithBadgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonWithBadgeApp.swift
+//  ButtonWithBadge
+//
+//  Created by atharvSharma on 22/01/22.
+//
+
+import SwiftUI
+
+@main
+struct ButtonWithBadgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
